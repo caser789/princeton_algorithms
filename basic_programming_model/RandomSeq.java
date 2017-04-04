eef36e36a8967c915d8dac88fd49f3fc8b33158c
Jiao Xue <jiao.xue@ihandysoft.com>
2017-04-04 19:34:52 +0800

    random sequence

---
+++ basic_programming_model/RandomSeq.java
@@ -0,0 +1,14 @@
+public class RandomSeq
+{
+    public static void main(String[] args)
+    {
+        int n = Integer.parseInt(args[0]);
+        double lo = Double.parseDouble(args[1]);
+        double hi = Double.parseDouble(args[2]);
+        for (int i=0; i<n; i++)
+        {
+            double x = StdRandom.uniform(lo, hi);
+            StdOut.printf("%.2f\n",x);
+        }
+    }
+}
